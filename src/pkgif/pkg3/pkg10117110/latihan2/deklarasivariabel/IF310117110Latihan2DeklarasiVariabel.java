596559188126f866831740f1eed4e40fd9c4b4cb
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 16:55:45 +0700

    version 1.0.0

---
+++ src/pkgif/pkg3/pkg10117110/latihan2/deklarasivariabel/IF310117110Latihan2DeklarasiVariabel.java
@@ -0,0 +1,38 @@
+
+package pkgif.pkg3.pkg10117110.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Jenis Variabel yang kita inginkan
+ */
+public class IF310117110Latihan2DeklarasiVariabel {
+
+
+    public static void main(String[] args) {
+        
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel Logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+
+    }
+    
+}
